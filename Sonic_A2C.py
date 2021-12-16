d8159ccd68bb725c7433fad0224bca3bcc6653a7
luthierman <dh2868@columbia.edu>
2021-12-15 19:45:32 -0500

    adding sonic A2C, and PPO

---
+++ Sonic_A2C.py
@@ -0,0 +1,42 @@
+import retro
+from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
+from stable_baselines3 import A2C
+from stable_baselines3.common.monitor import Monitor
+import wandb
+from wandb.integration.sb3 import WandbCallback
+
+config = {
+    "policy_type": "MlpPolicy",
+    "total_timesteps": 25000,
+    "env_name": "SonicTheHedgehog-Genesis",
+    "state": "GreenHillZone.Act1"
+}
+run = wandb.init(
+    project="test_rl",
+    config=config,
+    sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+    monitor_gym=True,  # auto-upload the videos of agents playing the game
+    save_code=True,  # optional
+)
+
+def make_env():
+    env = retro.make(config["env_name"], state=config["state"])
+    env = Monitor(env)  # record stats such as returns
+    return env
+
+env = DummyVecEnv([make_env])
+env = VecVideoRecorder(env, f"videos/{run.id}", record_video_trigger=lambda x: x % 2000 == 0, video_length=200)
+
+modelname = "sonic_ac2"
+model = A2C("CnnPolicy", env, verbose=1)
+model.set_env(env)
+model = A2C(config["policy_type"], env, verbose=1, tensorboard_log=f"runs/{run.id}")
+model.learn(
+    total_timesteps=config["total_timesteps"],
+    callback=WandbCallback(
+        gradient_save_freq=100,
+        model_save_path=f"models/{run.id}",
+        verbose=2,
+    ),
+)
+run.finish()
